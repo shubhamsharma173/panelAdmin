5b109ce78e93b96d47413d7caeddb5bf4078cae9
shubham <shubhamsharma173@gmail.com>
2021-04-13 13:13:30 +0530

    fix

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const cors = require('cors');
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const url = process.env.URL;
 
